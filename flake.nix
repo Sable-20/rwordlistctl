75203525fa8d040dc8b69252980929a84169cb62
TheOkayDev <72171476+Sable-20@users.noreply.github.com>
2024-04-11 13:39:42 -0400

    Try #3
    Realized I was trying to fix the devShell which was already working when it was the build command failing

--- flake.nix
+++ flake.nix
@@ -12,10 +12,15 @@
         naersk-lib = pkgs.callPackage naersk { };
       in
       {
-        defaultPackage = naersk-lib.buildPackage ./.;
+        defaultPackage = with pkgs; naersk-lib.buildPackage {
+          src = ./.;
+          nativeBuildInputs = [ pkg-config ];
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy openssl ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+        };
         devShell = with pkgs; mkShell {
           nativeBuildInputs = [ pkg-config ];
-          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy openssl openssl.dev ];
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy openssl ];
           # PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig";
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
           shellHook = ''
