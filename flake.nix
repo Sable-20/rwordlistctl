b8bc0688dbed60e67b7e0328c98f5b8baa212713
TheOkayDev <72171476+Sable-20@users.noreply.github.com>
2024-04-10 20:06:39 -0400

    Development environment work
    Started movement from Docker and local environments to nix so that development environments are reproducible as well as build environments. Cargo.lock was also added back so that crate versions can be locked.

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  inputs = {
+    naersk.url = "github:nix-community/naersk/master";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, utils, naersk }:
+    utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        naersk-lib = pkgs.callPackage naersk { };
+      in
+      {
+        defaultPackage = naersk-lib.buildPackage ./.;
+        devShell = with pkgs; mkShell {
+          buildInputs = [ cargo rustc rustfmt pre-commit rustPackages.clippy openssl pkg-config ];
+          RUST_SRC_PATH = rustPlatform.rustLibSrc;
+          shellHook = ''
+            echo "Entering devShell"
+            export PS1="\[\033[4;31m\](devSheel)\[\033[0;00m\] $PS1"
+          '';
+        };
+      }
+    );
+}
